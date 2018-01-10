5b1f91f64f8f1b8a22feccb4f455b4eb8a1c0c31
jhiggi44 <jordandhiggins@gmail.com>
2018-01-09 22:50:25 -0700

    re-style side-bar

--- assets/java/app.js
+++ assets/java/app.js
@@ -15,8 +15,15 @@ $(document).ready(function () {
   //initialize parallax
   $('.parallax').parallax();
 
- // Initialize collapse button
- $(".button-collapse").sideNav();
+  // Initialize collapse button
+  $(".button-collapse").sideNav();
+
+  $('.button-collapse').sideNav({
+    menuWidth: 400, // Default is 300
+    edge: 'left', // Choose the horizontal origin
+    closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+    draggable: true, // Choose whether you can drag to open on touch screens
+  });
 
 
 
